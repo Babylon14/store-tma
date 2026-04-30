ddc5291873e84cf794206ae13fbd90eaf025a85b
Andrew <andrkiy19@gmail.com>
2026-04-30 11:35:57 -0700

    Added schema for category

---
+++ backend/src/schemas/category_schema.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class CategoryBase(BaseModel):
+    name: str
+    icon_url: str | None = None
+    
+    
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class CategoryResponse(CategoryBase):
+    id: int
+
+
+    model_config = ConfigDict(from_attributes=True)
+
+
